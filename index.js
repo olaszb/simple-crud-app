d37c34c11adb1090ba2583a22d4072fd727601e1
gt47ob <gt47ob@inf.elte.hu>
2025-10-01 15:32:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import Product from './models/product.model.js';
+const app = express()
+
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node API');
+});
+
+app.get('/api/products', async (req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/products', async (req, res) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+})
+
+mongoose.connect('mongodb+srv://olaszg48_db_user:Olasbalint2@backenddb.lezy9sj.mongodb.net/simple-crud-app?retryWrites=true&w=majority&appName=BackendDB')
+.then(() => {
+    console.log("Connected to database!")
+    app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    })
+})
+.catch(() => {
+    console.log("Connection failed!")
+})
