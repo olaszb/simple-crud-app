036a49c55e9cd98ba58ea91e9d797cbf6172c651
gt47ob <gt47ob@inf.elte.hu>
2025-10-01 16:09:53 +0200

    Added a product update and delete request

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ app.get('/api/products', async (req,res) => {
     }
 })
 
+app.get('/api/products/:id', async (req,res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 app.post('/api/products', async (req, res) => {
     try{
         const product = await Product.create(req.body);
@@ -28,6 +38,44 @@ app.post('/api/products', async (req, res) => {
         res.status(500).json({message: error.message});
     }
 })
+
+//update product
+app.put('/api/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product){
+            return res.status(404).json({message: "Product not found"})
+        }
+
+        const updatedProduct = await Product.findById(id);
+
+        res.status(200).json(updatedProduct)
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+// delete a product
+
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product){
+            return response.status(404).json({message: "Product not found"})
+        }
+        res.status(200).json({message: "Product deleted successfully!"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 mongoose.connect('mongodb+srv://olaszg48_db_user:Olasbalint2@backenddb.lezy9sj.mongodb.net/simple-crud-app?retryWrites=true&w=majority&appName=BackendDB')
 .then(() => {
